82ddb47fcbec5dcfd035e9328cb7ee5c673bafc8
hanus hasl <hanus.hasl@mt.com>
2018-09-04 13:45:09 +0200

    Add initial header

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Unishop | Universal E-Commerce Template
+    </title>
+    <!-- SEO Meta Tags-->
+    <meta name="description" content="Bellashop - Sewing products shop">
+    <meta name="keywords" content="shop, e-commerce, sewing, bags, wallets">
+    <meta name="author" content="Hanus Hasl">
+  </head>
+  <!-- Body-->
+  <body>
