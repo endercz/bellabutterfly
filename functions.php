c796aa10ce9faa689109cadbb5fc7aa1a0a2fce8
hanus hasl <hanus.hasl@mt.com>
2018-09-07 15:52:17 +0200

    Shop page - woocommerce loop initial support

--- functions.php
+++ functions.php
@@ -13,3 +13,28 @@ register_nav_menus(array(
 ));
 
 require get_template_directory().'/inc/bellashop_nav_menu_walker.php';
+
+// Products loop customization
+remove_action('woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10);
+
+function bellashop_template_loop_product_link_open()
+{
+    global $product;
+
+    $link = apply_filters('woocommerce_loop_product_link', get_the_permalink(), $product);
+
+    echo '<a href="'.esc_url($link).'" class="product-thumb">';
+}
+add_action('woocommerce_before_shop_loop_item', 'bellashop_template_loop_product_link_open', 10);
+
+// Product thumbnails
+remove_action('woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10);
+
+function bellashop_template_loop_product_thumbnail()
+{
+    global $loop;
+    global $product;
+    $image = wp_get_attachment_image_src(get_post_thumbnail_id($loop->post->ID), 'single-post-thumbnail');
+    echo "<img src=$image[0] alt=\"Product\">";
+}
+add_action('woocommerce_before_shop_loop_item_title', 'bellashop_template_loop_product_thumbnail', 10);
--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -25,68 +25,73 @@ get_header('shop');
  * @hooked woocommerce_breadcrumb - 20
  * @hooked WC_Structured_Data::generate_website_data() - 30
  */
-do_action('woocommerce_before_main_content');
+//do_action('woocommerce_before_main_content');
 
 ?>
-<header class="woocommerce-products-header">
-	<?php if (apply_filters('woocommerce_show_page_title', true)) : ?>
-		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
-	<?php endif; ?>
+<!-- Shop Toolbar-->
+<div class="shop-toolbar padding-bottom-1x mb-2">
+    <div class="column">
+        <div class="shop-sorting">
+            <label for="sorting">Sort by:</label>
+            <select class="form-control" id="sorting">
+            <option>Popularity</option>
+            <option>Low - High Price</option>
+            <option>High - Low Price</option>
+            <option>Avarage Rating</option>
+            <option>A - Z Order</option>
+            <option>Z - A Order</option>
+            </select><span class="text-muted">Showing:&nbsp;</span><span>1 - 12 items</span>
+        </div>
+    </div>
+    <div class="column">
+    <div class="shop-view"><a class="grid-view" href="shop-grid-ls.html"><span></span><span></span><span></span></a><a class="list-view active" href="shop-list-ls.html"><span></span><span></span><span></span></a></div>
+    </div>
+</div>
 
-	<?php
-    /**
-     * Hook: woocommerce_archive_description.
-     *
-     * @hooked woocommerce_taxonomy_archive_description - 10
-     * @hooked woocommerce_product_archive_description - 10
-     */
-    do_action('woocommerce_archive_description');
-    ?>
-</header>
 <?php
 if (woocommerce_product_loop()) {
-        /*
-         * Hook: woocommerce_before_shop_loop.
-         *
-         * @hooked wc_print_notices - 10
-         * @hooked woocommerce_result_count - 20
-         * @hooked woocommerce_catalog_ordering - 30
-         */
-        do_action('woocommerce_before_shop_loop');
+    /*
+     * Hook: woocommerce_before_shop_loop.
+     *
+     * @hooked wc_print_notices - 10
+     * @hooked woocommerce_result_count - 20
+     * @hooked woocommerce_catalog_ordering - 30
+     */
+    do_action('woocommerce_before_shop_loop');
 
-        woocommerce_product_loop_start();
+    woocommerce_product_loop_start();
 
-        if (wc_get_loop_prop('total')) {
-            while (have_posts()) {
-                the_post();
+    if (wc_get_loop_prop('total')) {
+        while (have_posts()) {
+            the_post();
 
-                /*
-                 * Hook: woocommerce_shop_loop.
-                 *
-                 * @hooked WC_Structured_Data::generate_product_data() - 10
-                 */
-                do_action('woocommerce_shop_loop');
+            /*
+             * Hook: woocommerce_shop_loop.
+             *
+             * @hooked WC_Structured_Data::generate_product_data() - 10
+             */
+            do_action('woocommerce_shop_loop');
 
-                wc_get_template_part('content', 'product');
-            }
+            wc_get_template_part('content', 'product');
         }
-
-        woocommerce_product_loop_end();
-
-        /*
-         * Hook: woocommerce_after_shop_loop.
-         *
-         * @hooked woocommerce_pagination - 10
-         */
-        do_action('woocommerce_after_shop_loop');
-    } else {
-        /*
-         * Hook: woocommerce_no_products_found.
-         *
-         * @hooked wc_no_products_found - 10
-         */
-        do_action('woocommerce_no_products_found');
     }
+
+    woocommerce_product_loop_end();
+
+    /*
+     * Hook: woocommerce_after_shop_loop.
+     *
+     * @hooked woocommerce_pagination - 10
+     */
+    do_action('woocommerce_after_shop_loop');
+} else {
+    /*
+     * Hook: woocommerce_no_products_found.
+     *
+     * @hooked wc_no_products_found - 10
+     */
+    do_action('woocommerce_no_products_found');
+}
 
 /*
  * Hook: woocommerce_after_main_content.
