a19f59a1be0794fb726264f2da218df78b69e243
hanus hasl <hanus.hasl@mt.com>
2018-09-20 15:31:42 +0200

    Fix bellashop_nav_menu_walker undefined variable error

--- inc/bellashop_nav_menu_walker.php
+++ inc/bellashop_nav_menu_walker.php
@@ -40,7 +40,7 @@ class Bellashop_Nav_Walker extends Walker_Nav_Menu
         // $attributes = !empty($item->attr_title) ? ' title="'.esc_attr($item->attr_title).'"' : '';
         // $attributes .= !empty($item->target) ? ' target="'.esc_attr($item->target).'"' : '';
         // $attributes .= !empty($item->xfn) ? ' rel="'.esc_attr($item->xfn).'"' : '';
-        $attributes .= !empty($item->url) ? ' href="'.esc_attr($item->url).'"' : '';
+        $attributes = !empty($item->url) ? ' href="'.esc_attr($item->url).'"' : '';
 
         // $attributes .= ($args->walker->has_children) ? ' class="dropdown-toggle" data-toggle="dropdown"' : '';
 
